aa34195e5d733c69bb03ae9844a55ab15520f45c
MikiZenebe <mikizenebe10gmail.com>
2024-09-08 00:14:06 +0300

    ready to dokerize

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,4 @@
-export const getSongsUrl = "http://localhost:5000/api/song/";
-export const addSongsUrl = "http://localhost:5000/api/song/addSong";
-export const songUrl = "http://localhost:5000/api/song";
-export const statUrl = "http://localhost:5000/api/statistics";
+export const getSongsUrl = `${import.meta.env.VITE_API_URL}/api/song/`;
+export const addSongsUrl = `${import.meta.env.VITE_API_URL}/api/song/addSong`;
+export const songUrl = `${import.meta.env.VITE_API_URL}/api/song`;
+export const statUrl = `${import.meta.env.VITE_API_URL}/api/statistics`;
