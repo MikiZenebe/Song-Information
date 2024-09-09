2d2cc7d1a77406793db28a21df2d2fb29c135bd0
MikiZenebe <mikizenebe10gmail.com>
2024-09-09 21:42:58 +0300

    updated

--- server/src/db/connectDB.js
+++ server/src/db/connectDB.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.15"
-    );
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${connect.connection.host}`);
   } catch (error) {
     console.error(`Error connection to mongoDB: ${error.message}`);
